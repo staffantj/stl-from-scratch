2218e5108e002bfb5efe39055dc9019279e4a9a0
Staffan Tjernström <stjernstrom@acm.org>
2018-09-30 19:57:24 -0700

    After exercise 11

--- 11/a/count.h
+++ 11/a/count.h
@@ -1,12 +1,31 @@
 
 template<class It, class F>
-size_t count_if(It first, It last, const F& predicate)
+  typename It::difference_type count_if(It first, It last, const F& predicate)
 {
-    // YOUR CODE GOES HERE
+  typename It::difference_type count{0};
+  while( first != last )
+    {
+      if( predicate( *first ) )
+	{
+	  ++count;
+	}
+      ++first;
+    }
+
+  return count;
 }
 
 template<class It, class T>
-size_t count(It first, It last, const T& value)
+  typename It::difference_type count(It first, It last, const T& value)
 {
-    // YOUR CODE GOES HERE
+  typename It::difference_type result{0};
+  while( first != last )
+    {
+      if ( *first == value )
+	{
+	  ++result;
+	}
+      ++first;
+    }
+  return result;
 }
